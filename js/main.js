ae08e6a39729cf059d75a075f1a50cc1f764a0eb
linaahmed268 <85609621+linaahmed268@users.noreply.github.com>
2021-06-09 09:44:10 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var getUserName= prompt ("Hello! Who are u? Please state your name:");
+
+console.log(getUserName);
+
+document.getElementById("username").innerText = getUserName;
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
